7594f96d27ec5cc85a2db096ec26d0753b1240ca
js2493 <108201703+js2493@users.noreply.github.com>
2024-02-21 12:51:56 -0800

    change colors of boxes when mouse hovers over or clicks on one

--- script.js
+++ script.js
@@ -1,18 +1,61 @@
+const CELL_SIZE = "15px";
+const CELL_MARGIN = "0.25px";
+const NUM_CELLS = 50
 
-let body = document.body;
-for (let i = 0; i < 10; i++) {
+let container = document.createElement("div")
+container.style.display = "flex";
+container.style.flexDirection = "column";
+container.style.justifyContent = "center"
+container.style.alignItems = "center"
+container.style.height = "100vh"
+document.body.appendChild(container);
+
+let mouseDown = false;
+document.addEventListener("mousedown", () => {
+    mouseDown = true;
+});;
+
+document.addEventListener("mouseup", () => {
+    mouseDown = false;
+});
+
+// create cells
+for (let i = 0; i < NUM_CELLS; i++) {
     let row = document.createElement("div");
     row.classList.add("container");
     row.style.display = "flex";
-    row.style.margin = "3px 0"
-    for (let j = 0; j < 10; j++) {
+    row.style.margin = `${CELL_MARGIN} 0`
+    for (let j = 0; j < NUM_CELLS; j++) {
         let cell = document.createElement("div");
-        cell.style.backgroundColor = "blue";
-        cell.style.color = "red"
-        cell.style.width = "30px"
-        cell.style.height = "30px"
-        cell.style.margin = "0 3px"
+        cell.clicked = false;
+        cell.style.backgroundColor = "Gainsboro";
+        cell.style.width = CELL_SIZE
+        cell.style.height = CELL_SIZE
+        cell.style.margin = `0 ${CELL_MARGIN}`
+        cell.addEventListener("mouseenter", () => {
+            if (mouseDown) {
+                cell.style.backgroundColor = "red";
+                cell.clicked = true;
+            }
+            else if (!cell.clicked) { 
+                cell.style.backgroundColor = "black"; 
+            }
+        })
+        cell.addEventListener("mouseleave", () => {
+            if (!cell.clicked) {
+                setTimeout(() => {
+                    cell.style.backgroundColor = "Gainsboro";
+                }, 40);
+            }   
+        })
+        cell.addEventListener("mousedown", () => {
+            cell.style.backgroundColor = "red";
+            cell.clicked = true;
+        })
+        
+
         row.appendChild(cell);
     }
-    body.appendChild(row);
+    container.appendChild(row);
 }
+
